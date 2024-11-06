ba881bcec6962d77471a3a83d301cb77f2e0f3e6
RamonFranco <ramonccastro@hotmail.com>
2024-11-05 22:14:36 -0300

    Criando o controller e rota de transação

--- back-end/src/controllers/transacao.js
+++ back-end/src/controllers/transacao.js
@@ -1,17 +1,116 @@
 import prisma from '../database/client.js';
+import {includeRelations} from "../lib/utils.js";
+import req from "express/lib/request.js";
 
 const controller = {}
 
 controller.create = async (req, res) => {
     try {
+        const transacaoBody = req.body;
         await prisma.transacao.create({
-            data: req.body
+            data: {
+                valor: transacaoBody.valor,
+                descricao: transacaoBody.descricao,
+                data: transacaoBody.data,
+                metodo_pagamento: transacaoBody.metodo_pagamento,
+                tipo_transacao: transacaoBody.tipo_transacao,
+                categoria: {
+                    connect: {id: transacaoBody.categoria}
+                },
+                usuario: {
+                    connect: {id: transacaoBody.usuario},
+                }
+            }
         })
         res.status(201).end();
-    } catch (err){
+    } catch (err) {
         console.error(err);
         res.status(500).end()
     }
 }
 
+controller.retrieveAll = async (req, res) => {
+    try {
+        const include = includeRelations(req.query);
+        const result = await prisma.transacao.findMany({
+            include,
+            where: {
+                usuario_id: req.body.usuario
+            },
+            orderBy: [{data: 'desc'}]
+        })
+        res.status(200).send(result);
+    } catch (err) {
+        console.error(err);
+        req.status(500).send(err);
+    }
+}
+
+controller.retrieveOne = async (req, res) => {
+    try {
+        const include = includeRelations(req.query);
+        const result = await prisma.transacao.findUnique({
+            include,
+            where: {
+                id: req.params.id,
+                usuario_id: req.body.usuario,
+            },
+        })
+
+        if (!result) res.send(404).end();
+        else res.send(result);
+    } catch (err) {
+        console.error(err);
+        req.status(500).send(err);
+    }
+}
+
+controller.update = async (req, res) => {
+    try {
+        const transacaoBody = req.body;
+        const result = await prisma.transacao.update({
+            where: {id: req.params.id},
+            data: {
+                valor: transacaoBody.valor,
+                descricao: transacaoBody.descricao,
+                data: transacaoBody.data,
+                metodo_pagamento: transacaoBody.metodo_pagamento,
+                tipo_transacao: transacaoBody.tipo_transacao,
+                categoria: {
+                    connect: {id: transacaoBody.categoria}
+                },
+                usuario: {
+                    connect: {id: transacaoBody.usuario},
+                }
+            }
+        })
+        if (result) res.status(204).end();
+        else res.status(404).end();
+    } catch (err){
+        console.error(err);
+        res.status(500).send(err);
+    }
+}
+
+controller.delete = async (req, res) => {
+    try {
+        const transacao = await prisma.transacao.findFirst({
+            where: {
+                id: req.params.id,
+                usuario_id: req.body.usuario,
+            }
+        })
+        if (transacao) {
+            await prisma.transacao.delete({
+                where: {id: req.params.id}
+            })
+            res.status(204).end();
+        } else {
+            res.status(404).send({ message: "Categoria não encontrada ou usuário não autorizado." });
+        }
+    } catch (err){
+        console.error(err);
+        res.status(500).send(err);
+    }
+}
 export default controller;
--- back-end/src/routes/transacao.js
+++ back-end/src/routes/transacao.js
@@ -4,5 +4,9 @@ import controller from '../controllers/transacao.js'
 const router = Router()
 
 router.post('/', controller.create);
+router.get('/', controller.retrieveAll);
+router.get('/:id', controller.retrieveOne);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
 
 export default router;
